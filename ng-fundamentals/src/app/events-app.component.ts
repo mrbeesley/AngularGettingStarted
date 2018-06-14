59be68f7ec02bcfbaba4aca269370c2c6118fa1a
mrbeesley <mrbeesley@gmail.com>
2018-06-13 20:16:54 -0500

    inital commit to continue training on a different machine

---
+++ ng-fundamentals/src/app/events-app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-events',
+  template: `
+    <app-events-list></app-events-list>
+    `
+})
+export class EventsAppComponent {
+  title = 'app';
+}
