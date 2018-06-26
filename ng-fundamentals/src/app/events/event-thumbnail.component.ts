42dfd219872387f73cc88d5e82f016acc7280ecc
Michael Beesley <mrbeesley@gmail.com>
2018-06-25 20:35:44 -0500

    Through hiding divs with ngif and binding to hidden

--- ng-fundamentals/src/app/events/event-thumbnail.component.ts
+++ ng-fundamentals/src/app/events/event-thumbnail.component.ts
@@ -4,17 +4,26 @@ import { Component, Input,  } from '@angular/core';
     selector: 'event-thumbnail',
     template: `
     <div class="well hoverwell thumbnail">
-        <h2>{{event.name}}</h2>
-        <div>Date: {{event.date}}</div>
-        <div>Time: {{event.time}}</div>
-        <div>Price: \${{event.price}}</div>
+        <h2>{{event?.name}}</h2>
+        <div>Date: {{event?.date}}</div>
+        <div>Time: {{event?.time}}</div>
         <div>
-            <span>Location: {{event.location.address}}</span>
-            <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
+            <span>Early Start:</span>
+            <span>Late Start:</span>
+            <span>Normal Start:</span>
         </div>
+        <div>Price: \${{event?.price}}</div>
+        <div [hidden]="!event?.location">
+            <span>Location: {{event?.location?.address}}</span>
+            <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
+        </div>
+        <div *ngIf="event?.onlineUrl">
+            Online URL: {{event.onlineUrl}
+        }</div>
     </div>
     `,
     styles: [`
+        .thumbnail { min-height: 210px; }
         .pad-left { margin-left: 10px; }
         .well div { color: #bbb; }
     `]
